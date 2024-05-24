b5b07a12a0a5bd94243e1c17c4d8f41a710a510f
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2024-05-24 10:20:19 +0100

    Custom properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,14 @@ export default {
       colors: {
         "Very-dark-grayish": "#49557e",
         "light-grey": "#747474",
+        "medium-grey": "#676767",
         "lighter-tomato": "#fff4f2",
         "very-light-grey": "#e2e2e2",
         tomato: "#ff6347",
         "dark-greyish": "#262626",
         greyish: "#808080",
+        "bg-footer": "#323232",
+        "footer-color": "#d9d9d9",
       },
       fontFamily: {
         Outfit: ["Outfit", "sans-serif"],
@@ -22,6 +25,7 @@ export default {
         "dynamic-xl": "max(4.5vw,22px)",
         "dynamic-sm": "max(1vw,13px)",
         "dynamic-p": "max(1.4vw,16px)",
+        h2: "max(2vw,24px)",
       },
       keyframes: {
         fadeIn: {
@@ -31,6 +35,12 @@ export default {
       },
       animation: {
         fadeIn: "fadeIn 3s",
+      },
+      gridTemplateColumns: {
+        "auto-fill-240": "repeat(auto-fill, minmax(240px, 1fr))",
+      },
+      boxShadow: {
+        custom: "0px 0px 10px #00000015",
       },
     },
   },
