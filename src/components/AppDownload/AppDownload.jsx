593b6a2840d3cba28b767b8e9c84021cee93f58e
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2024-05-24 10:23:10 +0100

    Added AppDownload component

---
+++ src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,27 @@
+import { assets } from "../../assets/image/assets";
+
+export const AppDownload = () => {
+  return (
+    <div className="mx-auto mt-[100px] text-[max(3vw,20px)] text-center font-medium">
+      {/* app-download */}
+      <p>
+        For Better Experience Download <br />
+        Tomato App
+      </p>
+      <div className="flex flex-col items-center sm:flex-row sm:justify-center gap-[max(2vw,10px)] mt-10">
+        {/* app-download-platform */}
+        <img
+          className="w-[max(30vw,120px)] sm:max-w-[180px] self-stretch cursor-pointer transition duration-500 hover:scale-105"
+          src={assets.play_store}
+          alt="Play Store"
+        />
+        {/* w-40 sm:w-[180px] */}
+        <img
+          className="w-[max(30vw,120px)] max-w-[180px] self-stretch cursor-pointer transition duration-500 hover:scale-105"
+          src={assets.app_store}
+          alt="Apple Store"
+        />
+      </div>
+    </div>
+  );
+};
